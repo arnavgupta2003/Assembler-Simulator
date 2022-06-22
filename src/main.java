b961c03e376328b546fefe58c84d16f6a54bb5f0
Arnav Gupta <arnav.gupta.2003@gmail.com>
2022-06-22 21:04:38 +0530

    File I/O handled

--- src/main.java
+++ src/main.java
@@ -1,15 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException{
 		// TODO Main I/O file
+		
+		//Local I/O Code (Stdin)
 		Scanner sc = new Scanner(System.in);
 		String line = sc.nextLine().strip();
 		String[] in = line.split("\\s+");
-		for(String h:in) {
-			System.out.print(h+'_');
-		}
+//		for(String h:in) {
+//			System.out.print(h+'_');//Debug Code
+//		}
+		
+//		//File I/O Code
+//		File f = new File("");
+//		Scanner sc_f = new Scanner(f);
+//		while(sc_f.hasNextLine()) {
+//			String line_f = sc_f.nextLine().strip();
+//			String[] in_f = line.split("\\s+");
+//		}
+		
 
 	}
 }
