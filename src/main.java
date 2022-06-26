cbde585be4918a7a4bfc4fab6c43a4cce6e2d7dc
Arnav Gupta <arnav.gupta.2003@gmail.com>
2022-06-26 12:10:43 +0530

    Integrated returnReg.java with a function

--- src/main.java
+++ src/main.java
@@ -74,6 +74,28 @@ public class main {
             }
             return "Error";
     }
+	
+	 public static String returnReg(String regs){
+	        switch (regs){
+	            case "R0":
+	                return "000";
+	            case "R1":
+	                return "001";
+	            case "R2":
+	                return "010";
+	            case "R3":
+	                return "011";
+	            case "R4":
+	                return "100";
+	            case "R5":
+	                return "101";
+	            case "R6":
+	                return "110";
+	            case "FLAGS":
+	                return "111";
+	        }
+	        return "Error";
+	    }
 }
 
 
