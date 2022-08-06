06b881a2490e05416ae0a68e667232e7495d9358
Arnav Gupta <arnav.gupta.2003@gmail.com>
2022-08-06 23:25:40 +0530

    Ques_5 partial commit

--- src/Ques_5.py
+++ src/Ques_5.py
@@ -1,124 +1,200 @@
-def normal_query(instruction,register,space,addressed):
-    # tpye-a
-    i=0
-    while(P_address!=1):
-        P_address=space/2**i
-        i+=1
-    P_address=i
-    print(f"minimum bits are needed to represent an address in this architecture is {P_address}")
-    opcode_bits=instruction-register-P_address
-    print(f"Number of bits needed by opcode {opcode_bits}")
-    # type-b
-    filler_bit=instruction-2*register-opcode_bits
-    print(f"Number of filler bits is {filler_bit}")
-    max_instruction=space/instruction
-    printf(f"the maximum number of instructions this ISA can support is {len(str(int(max_instruction,2)))-1}")
-    max_register=space/register
-    printf(f"the maximum number of registers this ISA can support is {len(str(int(max_register,2)))-1}")
 
+dic={   "b":0,
+        "bit":0,
+        "Kb":10,
+        "Mb":20,
+        "Gb":30,
+        "kb":10,
+        "mb":20,
+        "gb":30,
+        
+        "byte":3,
+        "B":3,
+        "kB":13,
+        "mB":23,
+        "gB":33,
+        "KB":13,
+        "MB":23,
+        "GB":33,
 
+        
+        "Nibble":2,
+        "nibble":2,
+        
+        "knibble":12,
+        "kNibble":12,
+        "Knibble":12,
+        "KNibble":12,
+        
+        
+        "gnibble":22,
+        "gNibble":22,
+        "Gnibble":22,
+        "GNibble":22,
+    }
 
-def type1(cpu_bits,address_memory,space,addressed,retMemSize):
-    if(address_memory=="word_size"):
-        initial_pin=space/retMemSize(addressed)
-        current_pin=space/cpu_bits
-        pin_difference=len(str(int(initial_pin,2)))-1-len(str(int(current_pin,2)))-1
-        print(f"the number pin difference is {pin_difference}")
+def bincount(num):
+    a=0
+    while(num):
+        a+=1
+        num//=2
+    return a-1
+
+def ques_1(memSpace,memType):       
+    print("1. Type A: <Q bit opcode> <P-bit address> <N bit register>")
+    print("2. Type B: <Q bit opcode> <R bits filler> <N bit register> <N bit register>") 
+    isaType=int(input("Enter the index of the type of ISA(1/2):"))     
+    insLength=int(input("Length of one inst. in bits:"))
+    regLength=int(input("Length of one reg. in bits:"))
+    cpu=int(input("Enter the CPU size in bits:"))
+    meminbits=0
+    idx=0
+    inx=0
+    for i in memSpace:
+        if i.isdigit():
+            idx+=1
+            inx=inx*10+int(i)
+            continue
+    l=memSpace[idx:]
+    b=0
+    if l in ['word',"Word"]:
+        b=cpu
     else:
-        initial_pin = space / retMemSize(adddressed)
-        current_pin = space / retMemSize(address_memory)
-        pin_difference = len(str(int(initial_pin, 2))) - 1 - len(str(int(current_pin, 2))) - 1
-        print(f"the number pin difference is {pin_difference}")
+        b=2**dic[l.lower()]
 
-def type2(cpu_bits,address_pin,address_memory,retMemSize):
-    if (address_memory == "word_size"):
-        main_memory_size_power=(len(str(int(cpu_bits,2)))-1)+address_pin-3
-        main_memory_size=2**main_memory_size_power
-        print(f"the main memory size is {main_memory_size} bytes")
+    meminbits=inx*2**b         
+
+    if memType in ['word',"Word"]:
+        meminbits=cpu
     else:
-        main_memory_size_power = (len(str(int(retMemSize(address_memory), 2))) - 1) + address_pin - 3
-        main_memory_size = 2 ** main_memory_size_power
-        print(f"the main memory size is {main_memory_size} bytes")
+        meminbits=2**dic[memType.lower()]
 
+    if(isaType==1):
+        totalPins=bincount(memSpace/meminbits)
+        print(f"Minimum bits needed to represent an address in this architecture is: {totalPins}")
 
-retMemSize={"Bit":1,"Nibble":4,"Byte":8}
-# print("what is the type of your query?\n1)Normal Query\n2)Type 1\n3)Type 2\n Enter the number.")
-# type=input("enter the type number")
-# space=input("enter the space in memory")
-# addressed=input("enter how the memory is addressed")
+        opcode_bits=insLength-regLength-totalPins
+        print(f"Number of bits needed by opcode {opcode_bits}")
+        
+        max_instruction=2**opcode_bits
+        print(f"Maximum number of instructions this ISA can support is: {max_instruction}")
+        
+        max_register=2**regLength
+        print(f"Maximum number of registers this ISA can support is: {max_register}")
+    else:
 
-# if(type ==1):
-#     instruction=input("enter length of one instruction in bits")
-#     register=input("enter length of register in bits")
-#     normal_query(instruction,register,space,addressed)
-# elif(type==2):
-#     cpu_bits=input("how many bits is the cpu?")
-#     address_memory=input("input how you would want to change the current addressable memory to any of the rest 3 options")
-#     type1(cpu_bits,address_memory,space,addressed,retMemSize)
-# elif(type==3):
-#     cpu_bits=input("how many bits is the cpu?")
-#     address_pin=input("how many address pins?")
-#     address_memory=input("enter type of addressable memory")
-#     type2(cpu_bits,address_pin,address_memory,retMemSize)
+        totalPins=bincount(memSpace/meminbits)
+        print(f"Minimum bits needed to represent an address in this architecture is: {totalPins}")
+
+        opcode_bits=insLength-regLength-totalPins-regLength
+        print(f"Number of bits needed by opcode {opcode_bits}")
+
+        filler_bit=insLength-opcode_bits-2*regLength
+        print(f"Number of filler bits are: {filler_bit}")
+
+        max_instruction=2**opcode_bits
+        print(f"Maximum number of instructions this ISA can support is: {max_instruction}")
+        
+        max_register=2**regLength
+        print(f"Maximum number of registers this ISA can support is: {max_register}")
+
+def ques_2(memSpace,memType):    
+    quesType=int(input("Type of System Enhancement..(1/2) (0 to quit)?"))
+    cpu=int(input("Enter the CPU size in bits:"))
+    meminbits=0
+    idx=0
+    inx=0
+    for i in memSpace:
+        if i.isdigit():
+            idx+=1
+            inx=inx*10+int(i)
+            continue
+    l=memSpace[idx:]
+    b=0
+    if l in ['word',"Word"]:
+        b=cpu
+    else:
+        b=2**dic[l.lower()]
+
+    meminbits=inx*2**b         
+
+    if memType in ['word',"Word"]:
+        meminbits=cpu
+    else:
+        meminbits=2**dic[memType.lower()]
+
+    
+    if(quesType==1):   
+        cpuBits=cpu
+        meminbits=0
+        if memType in ['word',"Word"]:
+            meminbits=cpuBits
+        else:
+            meminbits=2**dic[memType.lower()]
+
+        initPins=bincount(memSpace/meminbits)
+
+        changeTo=input("Change the type of CPU to(Bit,Byte,Nibble,word):")
+
+        newmeminbits=0
+        if changeTo in ['word',"Word"]:
+            newmeminbits=cpuBits
+        else:
+            newmeminbits=2**dic[changeTo.lower()]
+
+        afterpins=bincount(memSpace/newmeminbits)
+
+        print("We need ",afterpins-initPins,"pins")
+
+    elif(quesType==2):
+        cpuBits=cpu
+        addPins=int(input("Address pins of CPU:"))
+        memType=input("Type of addressable memory(Bit/Byte/Nibble/word_add):")
+        meminbits=0
+        if memType in ['word',"Word"]:
+            meminbits=cpuBits
+        else:
+            meminbits=2**dic[memType.lower()]
+        
+        totalmem=addPins*meminbits
+        
+        expo=bincount(totalmem)
+
+        if (expo>30):
+            val=totalmem/(2**30)
+            print(val,"GigaBytes")
+        elif(expo>20):
+            val=totalmem/(2**20)
+            print(val,"MegaBytes")
+        elif(expo>10):
+            val=totalmem/(2**10)
+            print(val,"KiloBytes")
+        else:
+            print(val,"Bytes")
+    else:
+        print("Bye..")    
+
 def i_o():
     print("------------------------------------------")
     print("----------------Welcome-------------------")
     print("------------------------------------------")
-    print("Share initial input-->")
-    memSpace=input("Memory Space(x b/B/kB/mB):")
-    memType=input("Type of addressable memeory(Bit/Byte/Nibble/word_add):")
-    print("Queries start")
     while(True):
-        quesFlag=int(input("Enter the question type:"))
+        print("Share initial input-->")
+        memSpace=input("Memory Space:")
+        memType=input("Type of addressable memory(Bit/Byte/Nibble/word):")
+        print("Queries start")
+    
+        quesFlag=int(input("Which question should we try first(1/2)?"))
         if(quesFlag==1):
-            ques_1()
-        else:
-            ques_2() 
+            print()
+            ques_1(memSpace,memType)
+            print()
+        elif(quesFlag==2):
+            print()
+            ques_2(memSpace,memType) 
+            print()
+        cont=input("Continue?(0/1)")
+        if(cont=="0"):
+            break        
 
-
-
-
-    cont=input("Continue?(0/1)")
-    if(cont=="0"):
-        break
-
- def ques_1():       
-    print("1. Type A: <Q bit opcode> <P-bit address> <7 bit register>")
-    print("2. Type B: <Q bit opcode> <R bits filler> <7 bit register> <7 bit register>")    
-    isaType=int(input("What is the ISA Type(1/2):"))       
-    insLength=int(input("Length of one inst. in bits:"))
-    regLength=int(input("Lenght of one reg. in bits:"))
-
-    if(isaType==1):
-        insBits=input("Length of one inst. in bits:")
-        regLength=input("length of register in bits:")
-
-        # def normal_query(instruction,register,space,addressed):
-        # # tpye-a
-        # i=0
-        # while(P_address!=1):
-        #     P_address=space/2**i
-        #     i+=1
-        # P_address=i
-        # print(f"minimum bits are needed to represent an address in this architecture is {P_address}")
-        # opcode_bits=instruction-register-P_address
-        # print(f"Number of bits needed by opcode {opcode_bits}")
-        # # type-b
-        # filler_bit=instruction-2*register-opcode_bits
-        # print(f"Number of filler bits is {filler_bit}")
-        # max_instruction=space/instruction
-        # printf(f"the maximum number of instructions this ISA can support is {len(str(int(max_instruction,2)))-1}")
-        # max_register=space/register
-        # printf(f"Maximum number of registers this ISA can support is:{len(str(int(max_register,2)))-1}")
-    elif(isaType==2):
-
-def ques_2():    
-    quesType=int(input("Type of System Enhancement..?"))
-    if(quesType==1):   
-        cpuBits=int(input("Bits of CPU:"))
-        changeTo=input("Change the type of CPU to(Bit,Byte,Nibble,word_add):")
-    else:
-        cpuBits=int(input("Bits of CPU:"))
-        addPins=int(input("Address pins of CPU:"))
-        memType=input("Type of addressable memeory(Bit/Byte/Nibble/word_add):")
-        
+i_o() 
